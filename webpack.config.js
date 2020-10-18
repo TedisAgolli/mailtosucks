1c660eaf2d0df1ea2024b486205c4279d5b4bdad
TedisAgolli <tedisagolli@gmail.com>
2020-10-17 22:56:30 -0400

    Add temp logo

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: "manifest.json", to: "." }],
+      patterns: [
+        { from: "manifest.json", to: "." },
+        { from: "email.png", to: "." },
+      ],
     }),
   ],
   module: {
